e1076ccd304a4e9bd62045a3b5e48614376c6c98
asurowy <101614024+asurowy@users.noreply.github.com>
2022-07-26 22:19:23 -0400

    commit tree a4de111b5cbd5b4d940a3048f6f28a262ac0ca5a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+
+
+
+const returnFirstTwoDrivers = (function(drivers){
+const start = returnFirstTwoDrivers.slice(0,1)
+console.log(start)
+})
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])(start)
